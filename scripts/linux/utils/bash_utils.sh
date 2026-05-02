20aa9b78b4de97ba8aa008cdf6afd03919abd4a6
Yan Hermanny <88159884+yanhermanny@users.noreply.github.com>
2026-05-02 14:46:44 -0300

    Corrige utilizacao do logger

--- scripts/linux/utils/bash_utils.sh
+++ scripts/linux/utils/bash_utils.sh
@@ -7,7 +7,7 @@
 [[ -n "${__BASH_UTILS_SH_LOADED:-}" ]] && return
 readonly __BASH_UTILS_SH_LOADED=1
 
-# ---- source logger library -------------------------------------------------
+# ---- source logger library ---------------------------------------------------
 LOGGER_LIB="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)/logger.sh"
 if [[ ! -f "$LOGGER_LIB" ]]; then
   printf "\033[%sm%s\033[0m\n" "1;31" "❌ Required library not found: $LOGGER_LIB" >&2
@@ -31,7 +31,7 @@ sudo() {
     fi
   else
     if ! command sudo --non-interactive true 2>/dev/null; then
-      log_warning "Root privileges are required, please enter your password below"
+      log "Root privileges are required, please enter your password below" --level WARNING
       command sudo --validate
     fi
     if [[ "${exec}" == true ]]; then
